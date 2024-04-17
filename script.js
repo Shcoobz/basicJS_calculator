73490a0294fd4057dcf821902862f5e2d1abc872
Shcoobz <a.boeckmann23@gmail.com>
2024-04-17 21:12:47 +0200

    added calculation functionality

--- script.js
+++ script.js
@@ -28,21 +28,37 @@ function addDecimal() {
   }
 }
 
+// Calculate first and second values depending on operator
+const calculate = {
+  '/': (firstNumber, secondNumber) => firstNumber / secondNumber,
+  '*': (firstNumber, secondNumber) => firstNumber * secondNumber,
+  '+': (firstNumber, secondNumber) => firstNumber + secondNumber,
+  '-': (firstNumber, secondNumber) => firstNumber - secondNumber,
+  '=': (firstNumber, secondNumber) => secondNumber,
+};
+
 function useOperator(operator) {
   const currentValue = Number(calculatorDisplay.textContent);
+
+  // Prevent multiple operators
+  if (operatorValue && awaitingNextValue) {
+    operatorValue = operator;
+
+    return;
+  }
 
   // Assign first value, if no value
   if (!firstValue) {
     firstValue = currentValue;
   } else {
-    console.log('currentValue:', currentValue);
+    const calculation = calculate[operatorValue](firstValue, currentValue);
+    calculatorDisplay.textContent = calculation;
+    firstValue = calculation;
   }
 
   // Ready for next value, store operator
   operatorValue = operator;
   awaitingNextValue = true;
-
-  console.log('firstValue:', firstValue, 'operator:', operator);
 }
 
 // Add Event Listeners for numbers, operators, decimal button
